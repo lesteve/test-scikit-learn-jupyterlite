95fa989b53d8b689816a900dea8c2b3d981b31ea
Loïc Estève <loic.esteve@ymail.com>
2025-02-05 05:28:10 +0100

    Initial commit

---
+++ test_jupyterlite.py
@@ -0,0 +1,20 @@
+import pytest
+import time
+
+from playwright.sync_api import expect
+
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "https://scikit-learn.org/stable/lite/lab/index.html",
+        "https://scikit-learn.org/dev/lite/lab/index.html",
+    ],
+)
+def test_pyodide_kernel(page, url):
+    page.goto(url)
+    page.get_by_text("Python (Pyodide)").first.click()
+    page.get_by_role("textbox").locator("div").click()
+    page.keyboard.type('print("kernel ready")')
+    page.keyboard.press("Shift+Enter")
+    expect(page.get_by_text("kernel ready", exact=True)).to_be_visible(timeout=20_000)
