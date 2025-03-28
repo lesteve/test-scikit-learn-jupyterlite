803df478dfce897aa3c00a3a20def3088645ce62
Loïc Estève <loic.esteve@ymail.com>
2025-03-28 06:40:02 +0100

    Add retry mechanism

--- test_jupyterlite.py
+++ test_jupyterlite.py
@@ -12,11 +12,21 @@ from playwright.sync_api import expect
     ],
 )
 def test_pyodide_kernel(page, url):
-    page.goto(url)
-    page.get_by_text("Python (Pyodide)").first.click()
+    max_attempts = 10
+    for n_attempt in range(max_attempts):
+        try:
+            page.goto(url)
+            page.get_by_text("Python (Pyodide)").first.click()
+            break
+        except TimeoutError:
+            print(f"failure on attempt {n_attempt}")
+            time.sleep(5)
+            if n_attempt == max_attempt - 1:
+                raise
+
     time.sleep(5)
     page.get_by_role("textbox").locator("div").click()
-    page.keyboard.type('(20 + 25) ** 2')
+    page.keyboard.type("(20 + 25) ** 2")
     page.keyboard.press("Shift+Enter")
     expect(page.get_by_text("2025", exact=True)).to_be_visible(timeout=30_000)
     page.keyboard.type('print("kernel ready")')
